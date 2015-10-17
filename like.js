aea6992ee1efd9987ce74b31f22b6a86906af4ab
Phạm Gia Vang <phamgiavang@gmail.com>
2015-10-18 01:26:07 +0900

    Create like.js

---
+++ like.js
@@ -0,0 +1,26 @@
+ // Like
+$.fn.postLike = function() {
+    if ($(this).hasClass('done')) {
+        alert('Bạn đã thích bài viết');
+        return false;
+    } else {
+        $(this).addClass('done');
+        var id = $(this).data("id"),
+        action = $(this).data('action'),
+        rateHolder = $(this).children('.count');
+        var ajax_data = {
+            action: "specs_zan",
+            um_id: id,
+            um_action: action
+        };
+        $.post("/wp-admin/admin-ajax.php", ajax_data,
+        function(data) {
+            $(rateHolder).html(data);
+        });
+        return false;
+    }
+};
+$(document).on("click", ".specsZan",
+    function() {
+        $(this).postLike();
+});
